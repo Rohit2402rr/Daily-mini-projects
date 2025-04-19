52bda3c02f591fe673cc1bd40335820e0d048601
Rohit singh <159223527+Rohit2402rr@users.noreply.github.com>
2025-04-19 12:07:18 +0530

    "Social media slider menu"

---
+++ SM Selector Menu/script.js
@@ -0,0 +1,17 @@
+const menuE1 = document.querySelector(".menu");
+const menuTextE1 = document.querySelector(".menu p");
+const socialListsE1 = document.querySelector(".social-lists");
+const liE1 = document.querySelectorAll(".social-lists li");
+
+menuE1.addEventListener("click", () => {
+    socialListsE1.classList.toggle("hide");
+    menuE1.classList.toggle("rotate");
+});
+
+liE1.forEach((liE1) => {
+    liE1.addEventListener("click", () => {
+        menuTextE1.innerHTML = liE1.innerHTML;
+        socialListsE1.classList.add("hide");
+        menuE1.classList.toggle("rotate");
+    });
+});
